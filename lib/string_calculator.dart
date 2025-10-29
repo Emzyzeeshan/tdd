ef44c537e0b867da3a1d92f1497dfd3a6dde3dbf
Mohammed Zeeshan <55831823+Emzyzeeshan@users.noreply.github.com>
2025-10-30 01:57:36 +0530

    feat: handle two comma-separated numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,7 +1,26 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
+    return 0; // placeholder
+  }
+}
+
+
+class StringCalculatorSameNumber {
+  int add(String numbers) {
+    if (numbers.isEmpty) return 0;
     return int.parse(numbers);
   }
 }
 
+
+class StringCalculatorCommaSeparated {
+  int add(String numbers) {
+    if (numbers.isEmpty) return 0;
+
+    final parts = numbers.split(',');
+    if (parts.length == 1) return int.parse(parts[0]);
+
+    return parts.map(int.parse).reduce((a, b) => a + b);
+  }
+}
