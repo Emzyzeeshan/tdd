51b8f42bec90c14d0cad73be6b01957d5c5c8cb4
Mohammed Zeeshan <55831823+Emzyzeeshan@users.noreply.github.com>
2025-10-30 01:57:22 +0530

    test: add test for two comma-separated numbers

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -7,8 +7,12 @@ void main() {
     expect(calculator.add(''), equals(0));
   });
   test('returns the number itself when a single number is given', () {
-    final calculator = StringCalculator();
+    final calculator = StringCalculatorSameNumber();
     expect(calculator.add('1'), equals(1));
+  });
+  test('returns sum of two comma-separated numbers', () {
+    final calculator = StringCalculatorCommaSeparated();
+    expect(calculator.add('1,5'), equals(6));
   });
 
 }
